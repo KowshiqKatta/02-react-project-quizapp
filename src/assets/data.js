d27af0d286c6d2c9c736eb305f0c082659eab383
KowshiqKatta <kattamurikowshiq@gmail.com>
2024-08-11 07:49:57 +0530

    quiz today

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,13 +1,13 @@
-import Q1 from './problems/Q1.png';
-import Q2 from './problems/Q2.png';
-import Q3 from './problems/Q3.png';
-import Q4 from './problems/Q4.png';
-import Q5 from './problems/Q5.png';
-import Q6 from './problems/Q6.png';
-import Q7 from './problems/Q7.png';
-import Q8 from './problems/Q8.png';
-import Q9 from './problems/Q9.png';
-import Q10 from './problems/Q10.png';
+import Q1 from './problems/Q1.jpg';
+import Q2 from './problems/Q2.jpg';
+import Q3 from './problems/Q3.jpg';
+import Q4 from './problems/Q4.jpg';
+import Q5 from './problems/Q5.jpg';
+import Q6 from './problems/Q6.jpg';
+import Q7 from './problems/Q7.jpg';
+import Q8 from './problems/Q8.jpg';
+import Q9 from './problems/Q9.jpg';
+import Q10 from './problems/Q10.jpg';
 
 export const data = [
     {
@@ -16,7 +16,7 @@ export const data = [
       option2: "Option b",
       option3: "Option c",
       option4: "Option d",
-      ans: 2,
+      ans: 1,
     },
     {
       image: Q2,
@@ -32,7 +32,7 @@ export const data = [
       option2: "Option b",
       option3: "Option c",
       option4: "Option d",
-      ans: 3,
+      ans: 4,
     },
     {
       image: Q4,
@@ -40,7 +40,7 @@ export const data = [
       option2: "Option b",
       option3: "Option c",
       option4: "Option d",
-      ans: 4,
+      ans: 2,
     },
     {
       image: Q5,
@@ -52,42 +52,42 @@ export const data = [
     },
     {
       image: Q6,
-      option1: "Option a",
-      option2: "Option b",
-      option3: "Option c",
-      option4: "Option d",
-      ans: 2,
-    },
-    {
-      image: Q7,
-      option1: "Option a",
-      option2: "Option b",
-      option3: "Option c",
-      option4: "Option d",
-      ans: 3,
-    },
-    {
-      image: Q8,
-      option1: "Option a",
-      option2: "Option b",
-      option3: "Option c",
-      option4: "Option d",
+      option1: "ucos(theta)",
+      option2: "usin(theta)",
+      option3: "utan(theta)",
+      option4: "ucot(theta)",
       ans: 1,
     },
     {
+      image: Q7,
+      option1: "1 meter",
+      option2: "2 meter",
+      option3: "3 meter",
+      option4: "4 meter",
+      ans: 2,
+    },
+    {
+      image: Q8,
+      option1: "1 m/s",
+      option2: "2 m/s",
+      option3: "3 m/s",
+      option4: "4 m/s",
+      ans: 2,
+    },
+    {
       image: Q9,
-      option1: "Option a",
-      option2: "Option b",
-      option3: "Option c",
-      option4: "Option d",
-      ans: 3,
+      option1: "3rd bench",
+      option2: "4th bench",
+      option3: "5th bench",
+      option4: "6th bench",
+      ans: 4,
     },
     {
       image: Q10,
-      option1: "Option a",
-      option2: "Option b",
-      option3: "Option c",
-      option4: "Option d",
-      ans: 4,
+      option1: "True",
+      option2: "False",
+      option3: "Need some more inputs",
+      option4: "None of the above",
+      ans: 1,
     },
   ];
